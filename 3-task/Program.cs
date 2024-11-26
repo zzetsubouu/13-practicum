03f71b42c598b39308899ca2d4722ac1635e81a4
zzetsubouu <pyanix.mladshiy@gmail.com>
2024-11-26 22:02:19 +0200

    3 задание

---
+++ 3-task/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3_task
+{
+    /// <summary>
+    /// Задача 3. Найти s=min(a, b) + min(c, d), 
+    /// используя вспомогательные функции (ввода аргументов, нахождения минимальной величины, вывода результата).
+    /// </summary>
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = Input("a=");
+            int b = Input("b=");
+            int c = Input("c=");
+            int d = Input("d=");
+
+            Output(FindMinValue(a, b) + FindMinValue(c, d));
+            Console.ReadKey();
+        }
+        static int Input(string msg)
+        {
+            Console.Write(msg);
+            return int.Parse(Console.ReadLine());
+
+        }
+        static void Output(int result)
+        {
+            Console.WriteLine($"SumOfMin = {result}");
+        }
+        static int FindMinValue(int v1, int v2)
+        {
+            return v1 < v2 ? v1 : v2;
+        }
+    }
+}
---
+++ 3-task/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("3-task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("3-task")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("db56b485-aa0d-42f9-b074-cf44c4e4a0eb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
