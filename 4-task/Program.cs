cabd107b99f9d8e5c95e3253a560bb665f43ae06
zzetsubouu <pyanix.mladshiy@gmail.com>
2024-11-26 22:10:06 +0200

    4 задание

---
+++ 4-task/Program.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4_task
+{
+    /// <summary>
+    /// Задача 4. Определить статический метод int Seconds (int h, int m, int s), который принимает три целых аргумента 
+    /// (часы h, минуты m и секунды s) и возвращает количество секунд,
+    /// прошедших с начала дня. Используя этот метод, вычислить количество секунд, прошедших с начала дня при:
+
+    ///а) 0 ч 40 мин 20 с;
+
+    ///
+    /// </summary>
+    internal class Program
+    {
+        //а) 0 ч 40 мин 20 с
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    int hours = 0;
+        //    int mins = 40;
+        //    int seconds = 20;
+
+        //    Output(Seconds(hours, mins, seconds));
+        //    Console.ReadKey();
+        //}
+        //static void Output(int result)
+        //{
+        //    Console.WriteLine($"Секунд с начала дня = {result}с");
+        //}
+        //static int Seconds(int hours, int mins, int seconds)
+        //{
+        //    int totalSeconds = hours * 3600 + mins * 60 + seconds;
+        //    return totalSeconds;
+        //}
+
+
+
+
+
+
+        //б) 3 ч 10 мин 50 с.
+
+
+        static void Main(string[] args)
+        {
+            int hours = 3;
+            int mins = 10;
+            int seconds = 50;
+
+            Output(Seconds(hours, mins, seconds));
+            Console.ReadKey();
+        }
+        static void Output(int result)
+        {
+            Console.WriteLine($"Секунд с начала дня = {result}с");
+        }
+        static int Seconds(int hours, int mins, int seconds)
+        {
+            int totalSeconds = hours * 3600 + mins * 60 + seconds;
+            return totalSeconds;
+        }
+
+
+
+
+
+
+
+        //гибкая программа
+
+
+
+        //static void Main(string[] args)
+        //{
+        //    int hours = Input("Сколько часов: ");
+        //    int mins = Input("СКолько минут: ");
+        //    int seconds = Input("Сколько секунд: ");
+
+        //    Output(Seconds(hours, mins, seconds));
+        //    Console.ReadKey();
+        //}
+        //static int Input(string msg)
+        //{
+        //    return int.Parse(Console.ReadLine());
+
+        //}
+        //static void Output(int result)
+        //{
+        //    Console.WriteLine($"Секунд с начала дня = {result}с");
+        //}
+        //static int Seconds(int hours, int mins, int seconds)
+        //{
+        //    int totalSeconds = hours * 3600 + mins * 60 + seconds;
+        //    return totalSeconds;
+        //}
+    }
+}
---
+++ 4-task/Properties/AssemblyInfo.cs
@@ -0,0 +1,33 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("4-task")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("4-task")]
+[assembly: AssemblyCopyright("Copyright ©  2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("d530d6cb-8d58-4650-87c3-d397dff77cbb")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
